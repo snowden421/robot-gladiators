ead2369c842c96cbe7bd5e62768777fd253b48c5
Snow <84701509+snowden421@users.noreply.github.com>
2021-06-28 18:11:47 -0700

    added loop to fight sequence, made sure player fights all robots instead of just one

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,10 +15,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    // repeat and execute as long as the enemy-robot is alive
+    while (enemyHealth > 0) {
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -67,8 +66,12 @@ var fight = function(enemyName) {
     } else {
         window.alert("You need to chose a valid option. Try again!");
     }
+    }
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName =enemyNames[i];
+    enemyHealth = 50;
+    // call fight function with enemy-robot
+    fight(pickedEnemyName);
 }
